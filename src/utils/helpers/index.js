d91c41da5ca28f1a306e7922c24c1f73ea33d165
Angel Arsenio Reyes Espinal <angel.reyes@solvex.com.do>
2020-11-21 21:32:06 -0400

    first commit

---
+++ src/utils/helpers/index.js
@@ -0,0 +1,11 @@
+import moment from 'moment';
+const querystring = require('querystring');
+
+const convertDate = object => moment(object).format('LL');
+
+const isOnlyNumbers = value => /^\d+$/.test(value);
+
+const convertToQueryString = object => querystring.stringify(object);
+
+export default { convertDate, isOnlyNumbers, convertToQueryString };
+
